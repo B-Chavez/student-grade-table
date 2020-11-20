3c7ce98828bcc8c040107b3a79d4a34ee736bcdf
b-chavez <brandedhard@gmail.com>
2020-11-19 16:44:24 -0800

    added ajax get

--- components/app.js
+++ components/app.js
@@ -1,3 +1,24 @@
 class App{
-
+    constructor(
+      handleGetGradesError.bind(this);
+      handleGetGradesSuccess.bind(this);
+    )
+    handleGetGradesError (error){
+      console.error(error)
+    }
+    handleGetGradesSuccess (grades){
+      console.log(grades)
+    }
+    getGrades () {
+      $.ajax({
+        method: 'GET',
+        URL: 'https://github.com/Learning-Fuze/api/grades',
+        success: function () {
+          handleGetGradesSuccess()
+        }
+        error: function () {
+          handleGetGradesError()
+        }
+      })
+    }
 }
