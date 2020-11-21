97c2d797437957230c25d18580a230629fdfdf5b
b-chavez <brandedhard@gmail.com>
2020-11-21 13:14:38 -0800

    added JS to create a table

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,17 @@ class GradeTable{
         this.tableElement = tableElement;
     }
     updateGrades(grades){
-        console.log(grades);
+        var tbody = document.querySelector("tbody");
+        for(var i = 0; i < grades.length; i++){
+             var createTR = document.createElement("tr");
+             var createTDCol1 = document.createElement("td");
+             var createTDCol2 = document.createElement("td");
+             var createTDCol3 = document.createElement("td");
+             createTDCol1.append(grades[i].name);
+             createTDCol2.append(grades[i].course);
+             createTDCol3.append(grades[i].grade)
+             createTR.append(createTDCol1, createTDCol2, createTDCol3);
+             tbody.append(createTR)
+        }
     }
 }
