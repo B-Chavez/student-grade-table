cd849ed36eab61b2141f14a04d4099aedc84e3c3
b-chavez <brandedhard@gmail.com>
2020-11-21 15:16:50 -0800

    instantiated PageHeader

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var myTableEl = document.getElementsByName("table");
+var myHeaderEl = document.querySelector("#header");
+
+var myPageHeader = new PageHeader(myHeaderEl);
 var myGradeTable = new GradeTable(myTableEl);
 var myApp = new App(myGradeTable);
 
