fef4cea0253c4fb45e8d8dcfc05d749dbef7409b
b-chavez <brandedhard@gmail.com>
2020-11-24 20:42:26 -0800

    finished GradeForm component

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var myTableEl = document.getElementsByName("table");
 var myHeaderEl = document.querySelector("span");
+var myForm = document.querySelector("form");
 
+var gradeForm = new GradeForm(myForm);
 var myPageHeader = new PageHeader(myHeaderEl);
 var myGradeTable = new GradeTable(myTableEl);
 var myApp = new App(myGradeTable, myPageHeader);
