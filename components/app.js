bb5a2149a363195474b86a23f99e1736d53c6158
b-chavez <brandedhard@gmail.com>
2020-12-01 20:37:55 -0800

    added methods to app.js DeleteGrade, handleDeleteGradeError, handleDeleteGradeSuccess

--- components/app.js
+++ components/app.js
@@ -9,6 +9,10 @@ class App{
       this.createGrade = this.createGrade.bind(this);
       this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
       this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+
+      this.deleteGrade = this.deleteGrade.bind(this);
+      this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
+      this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
     }
     handleGetGradesError (error){
       console.error(error);
@@ -59,6 +63,18 @@ class App{
     }
 
     handleCreateGradeSuccess(){
-      this.getGrades()
+      this.getGrades();
+    }
+
+    deleteGrade(id){
+      console.log(id);
+    }
+
+    handleDeleteGradeError(error){
+      console.error(error);
+    }
+
+    handleDeleteGradeSuccess(){
+      this.getGrades();
     }
 }
