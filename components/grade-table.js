3cfdeedda8614175667c0b7fab96ab475fe8346e
b-chavez <brandedhard@gmail.com>
2020-12-01 21:09:22 -0800

    created Elements for table row

--- components/grade-table.js
+++ components/grade-table.js
@@ -19,4 +19,17 @@ class GradeTable{
     onDeleteClick(deleteGrade){
         this.deleteGrade = deleteGrade;
     }
+
+    renderGradeRow(data, deleteGrade){
+            var createTR = document.createElement("tr");
+            var createTDCol1 = document.createElement("td");
+            var createTDCol2 = document.createElement("td");
+            var createTDCol3 = document.createElement("td");
+            var createTDCol4 = document.createElement("td");
+            var createButton = document.createElement("button");
+
+            createButton.classList = "btn-danger"
+            createTDCol4.append(createButton);
+            createTR.append(createTDCol1, createTDCol2, createTDCol3, createTDCol4);
+    }
 }
