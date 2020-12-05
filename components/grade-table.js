de3977b31239b1f4f89adb5f17e9a9bff3cfd9f8
b-chavez <brandedhard@gmail.com>
2020-12-04 21:33:21 -0800

    removed a dsecond add event listener

--- components/grade-table.js
+++ components/grade-table.js
@@ -10,11 +10,11 @@ class GradeTable{
              tbody.append(row);
 
         }
-        console.log(grades)
-        if (grades.length === 0){
-            document.querySelector("p").classList.remove("d-none");
-        } else {
+        if (grades){
             document.querySelector("p").classList.add("d-none");
+        } else {
+            document.querySelector("p").classList.remove("d-none");
+            
         }
 
     }
@@ -48,10 +48,6 @@ class GradeTable{
                 deleteGrade(data.id)
             });
 
-
-            createButton.addEventListener('click', function(){
-                deleteGrade(data.id)
-            });
             return createTR;
     }
 
